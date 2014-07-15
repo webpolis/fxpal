e17248e7c2e738b0724e4354890c62bf92254111
Nicolas Iglesias <nico@webpolis.com.ar>
2014-07-15 05:42:41 -0300

    add yql tickers

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -32,12 +32,68 @@ angular.module('aifxApp').controller('appController', function($scope, $ionicSid
             weeklyChange: 'http://www.quandl.com/api/v1/multisets.json?columns={{sets}}&collapse=weekly&auth_token={{token}}&rows=1&transformation=rdiff&rows=4',
             yahooIndex: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quoteslist%20where%20symbol%20in%20({{quotes}})&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
             /* events: 'http://calendar.fxstreet.com/eventdate/mini?f=json&culture=en-US&rows=165&pastevents=100&hoursbefore=240&timezone=Argentina+Standard+Time&columns=date%2Ctime%2Cevent%2Cconsensus%2Cprevious%2Cactual&showcountryname=false&countrycode=AU%2CCA%2CJP%3AEMU%2CNZ%2CCH%2CUK%2CUS&isfree=true'*/
+            /*
+            select * from yahoo.finance.quoteslist where symbol in ('^GDAXI','^FTSE','^AORD','^N225','^GSPTSE')
+             */
             events: 'http://www.dailyfx.com/files/Calendar-{{startWeekDate}}.csv'
         },
         // used for multiset query
         multiVariables: ['OFDP.FUTURE_B1.1', 'WGC.GOLD_DAILY_USD.1', 'WSJ.CORN_2.1', 'OFDP.SILVER_5.1', 'WSJ.PL_MKT.1', 'WSJ.COPPER.1', 'WSJ.FE_TJN.1', 'WSJ.ZINC.1', 'NIKKEI.INDEX.4', 'YAHOO.INDEX_AORD.4', 'YAHOO.INDEX_GSPTSE.4', 'YAHOO.INDEX_GDAXI.4', 'YAHOO.INDEX_FTSE.4', 'BCB.UDJIAD1.1' /*, 'RATEINF.CPI_USA.1', 'RATEINF.CPI_JPN.1', 'RATEINF.CPI_DEU.1', 'RATEINF.CPI_FRA.1', 'RATEINF.CPI_GBR.1', 'RATEINF.CPI_ITA.1', 'RATEINF.CPI_RUS.1', 'RATEINF.CPI_CAN.1', 'RATEINF.CPI_AUS.1'*/ ],
         correlation: {
             min: 0.71
+        },
+        yqls: {
+            indexes: 'select * from yahoo.finance.quoteslist where symbol in ({{sets}})',
+            quotes: 'select * from yahoo.finance.quote where symbol in ({{sets}})'
+        },
+        // keys are tickers used by yql
+        maps: {
+            tickers: {
+                'YAHOO.INDEX_GDAXI': {
+                    symbol: 'GDAXI',
+                    name: 'DAX'
+                },
+                'YAHOO.INDEX_FTSE': {
+                    symbol: 'FTSE',
+                    name: 'FTSE'
+                },
+                'YAHOO.INDEX_AORD': {
+                    symbol: 'AORD',
+                    name: 'AORD'
+                },
+                'NIKKEI.INDEX': {
+                    symbol: 'N225',
+                    name: 'NIKKEI'
+                },
+                'YAHOO.INDEX_GSPTSE': {
+                    symbol: 'GSPTSE',
+                    name: 'GSPTSE'
+                },
+                'OFDP.SILVER_5': {
+                    symbol: 'SIN14.CMX',
+                    name: 'SILVER'
+                },
+                'WGC.GOLD_DAILY_USD': {
+                    symbol: 'GCN14.CMX',
+                    name: 'GOLD'
+                },
+                'WSJ.COPPER': {
+                    symbol: 'HGN14.CMX',
+                    name: 'COPPER'
+                },
+                'WSJ.CORN_2': {
+                    symbol: 'CU14.CBT',
+                    name: 'CORN'
+                },
+                'WSJ.PL_MKT': {
+                    symbol: 'PLN14.NYM',
+                    name: 'PLATINUM'
+                },
+                'OFDP.FUTURE_B1': {
+                    symbol: 'CLQ14.NYM',
+                    name: 'OIL'
+                }
+            }
         }
     };
     $scope.toggleLeft = function() {
