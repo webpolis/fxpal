365a00ffcbd0893d6ffa98c6f4927871c51e3cd0
Nicolas Iglesias <nico@webpolis.com.ar>
2014-07-18 02:44:19 -0300

    add instructions

--- server/scripts/merge.js
+++ server/scripts/merge.js
@@ -3,13 +3,20 @@ var fs = require('fs'),
     pathDatasets = __dirname + '/../../app/data/';
 var datasets = fs.readdirSync(pathDatasets),
     calendars = [];
+/**
+ * collect datasets
+ */
 if (datasets.length > 0) {
     datasets.forEach(function(d, i) {
+        // calendars
         if (/^calendar.*\.csv/gi.test(d)) {
             calendars.push(pathDatasets + d);
         }
     });
 }
+/**
+ * merge calendars
+ */
 calendars.sort(function(a, b) {
     var dateA = new Date(a.replace(/.*(\d{2}\-\d{2}\-\d{4}).*/, '$1'));
     var dateB = new Date(b.replace(/.*(\d{2}\-\d{2}\-\d{4}).*/, '$1'));
@@ -20,4 +27,3 @@ calendars.sort(function(a, b) {
     }
     return 0;
 });
-
