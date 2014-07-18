227319296d63716c919455e56ab249baee5fae2b
Nicolas Iglesias <nico@webpolis.com.ar>
2014-07-18 00:00:31 -0300

    script fixes

--- server/scripts/calendar.sh
+++ server/scripts/calendar.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for i in $(node calendar.js) ;
-	do sleep 1;
-	curl -G -A "Mozilla/5.0 (Windows NT 6.1; rv:19.0) Gecko/20100101 Firefox/19.0" -O $i ;
-	mv Calendar*.csv ../../app/data/ ;
+for i in $(node calendar.js)
+	do
+		sleep 1
+		curl -G -A "Mozilla/5.0 (Windows NT 6.1; rv:19.0) Gecko/20100101 Firefox/19.0" -O $i
 done
+
+mv Calendar*.csv ../../app/data/
