7dcdfd8fe9c2a080e1381fd6bab1cd66acd589fa
Nicolas Iglesias <nico@webpolis.com.ar>
2014-07-18 01:07:09 -0300

    optimize rapidminer output for multisets correlation

--- server/scripts/calendar.sh
+++ server/scripts/calendar.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+CURDIR=$(dirname "$0")
 for i in $(node calendar.js)
 	do
 		sleep 1
 		curl -G -A "Mozilla/5.0 (Windows NT 6.1; rv:19.0) Gecko/20100101 Firefox/19.0" -O $i
 done
 
-mv Calendar*.csv ../../app/data/
+mv Calendar*.csv $CURDIR/../../app/data/
