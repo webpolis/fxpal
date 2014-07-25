ca7030c3d1b6a2ab09657d446ba41f23fa053ace
Nicolas Iglesias <nico@webpolis.com.ar>
2014-07-25 17:26:19 -0300

    make function in R

--- server/scripts/candlesticks.r
+++ server/scripts/candlesticks.r
@@ -5,30 +5,35 @@ library("rjson")
 library("candlesticks")
 
 opts = commandArgs(trailingOnly = TRUE)
-oandaToken = 'ce6b72e81af59be0bbc90152cad8d731-03d41860ed7849e3c4555670858df786'
 startDate = ifelse((exists("opts") && !is.na(opts[1])), opts[1], "2014-07-24")
 instrument = sub("(\\w{3})(\\w{3})", "\\1_\\2", ifelse((exists("opts") && !is.na(opts[2])), opts[2], "USDCAD"))
 granularity = ifelse((exists("opts") && !is.na(opts[3])), opts[3], "M")
 type = ifelse((exists("opts") && !is.na(opts[4])), opts[4], "trend")
 
-urlPractice = paste("https://api-fxpractice.oanda.com/v1/candles?instrument=", instrument, "&granularity=", granularity, "&start=", startDate, "&weeklyAlignment=Monday", sep = "")
+getCandles <- function(instrument, granularity, startDate){
+	oandaToken = 'ce6b72e81af59be0bbc90152cad8d731-03d41860ed7849e3c4555670858df786'
+	urlPractice = paste("https://api-fxpractice.oanda.com/v1/candles?instrument=", instrument, "&granularity=", granularity, "&start=", startDate, "&weeklyAlignment=Monday", sep = "")
 
-print(urlPractice)
+	print(urlPractice)
 
-ret = fromJSON(getURL(url = urlPractice, httpheader = c(Authorization = paste("Bearer ", oandaToken))))
+	ret = fromJSON(getURL(url = urlPractice, httpheader = c(Authorization = paste("Bearer ", oandaToken))))
 
-out = NULL
-for(c in 1:length(ret$candles)){
-	candle = as.data.frame(ret$candles[c])
-	rbind(out, candle) -> out
+	out = NULL
+	for(c in 1:length(ret$candles)){
+		candle = as.data.frame(ret$candles[c])
+		rbind(out, candle) -> out
+	}
+
+	out = out[,-(grep("[a-z]+Bid|complete",names(out)))]
+	rownames(out) = out[,1]
+	out = out[,-1]
+	names(out) = c("Open","High","Low","Close","Volume")
+	rownames(out) = as.POSIXlt(gsub("T|\\.\\d{6}Z", " ", rownames(out)))
+	out = as.xts(out)
+	return(out)
 }
 
-out = out[,-(grep("[a-z]+Bid|complete",names(out)))]
-rownames(out) = out[,1]
-out = out[,-1]
-names(out) = c("Open","High","Low","Close","Volume")
-rownames(out) = as.POSIXlt(gsub("T|\\.\\d{6}Z", " ", rownames(out)))
-out = as.xts(out)
+out = getCandles(instrument, granularity, startDate)
 
 if(type == "trend"){
 	trend = TrendDetectionChannel(out)
