49d2bc47b86cf00e1fb0bf0f434a17a3aaf1adf7
Nicolas Iglesias <nico@webpolis.com.ar>
2014-10-17 16:17:03 -0300

    calc open interest; graph cot stats

--- server/scripts/cftc.r
+++ server/scripts/cftc.r
@@ -8,22 +8,30 @@ tmp = tempfile()
 download.file(urlFinal,tmp)
 unzip(tmp,files=c(reportName))
 data = read.table(reportName, sep = ",", dec = ".", strip.white = TRUE, header=TRUE, encoding = "UTF-8")
-data = subset(data,CFTC.Market.Code.in.Initials=="CME")
+data = subset(data,CFTC.Market.Code.in.Initials=="CME"|CFTC.Market.Code.in.Initials=="ICUS")
 data[,1] = gsub("(.*)\\s+\\-\\s+.*","\\1",data[,1],ignore.case=T,perl=T)
 
 getPosition <- function(currency){
 	curr = subset(data,Market.and.Exchange.Names==currency)
+	oInterest = diff(rev(curr$Open.Interest..All.))
 	longNC = diff(rev(curr$Noncommercial.Positions.Long..All.))
 	shortNC = diff(rev(curr$Noncommercial.Positions.Short..All.))
 	ret = as.data.frame(merge(last(longNC),last(shortNC)))
+	ret = merge(ret,as.data.frame(last(oInterest)))
 	rownames(ret) = c(currency)
-	names(ret) = c("long","short")
+	names(ret) = c("long","short","interest")
 	return(ret)
 }
 
-currencies = c("CANADIAN DOLLAR","SWISS FRANC","BRITISH POUND STERLING","JAPANESE YEN","EURO FX","BRAZILIAN REAL","NEW ZEALAND DOLLAR","AUSTRALIAN DOLLAR")
+currencies = c("CANADIAN DOLLAR","SWISS FRANC","BRITISH POUND STERLING","JAPANESE YEN","EURO FX","BRAZILIAN REAL","NEW ZEALAND DOLLAR","AUSTRALIAN DOLLAR", "U.S. DOLLAR INDEX")
 
 stats = Reduce(rbind,lapply(currencies,getPosition))
 
 unlink(tmp)
 unlink(reportName)
+
+plot.ts(stats,plot.type=c("single"),col=c("green","red","black"),axes=F,xlab="currency")
+axis(1,at=1:9,labels=row.names(stats),cex.axis=0.5)
+axis(2,cex.axis=0.5,at=seq(min(stats),max(stats),by=5000))
+legend("topright", c("long","short","interest"), cex=0.8,col=c("green","red","black"), lty=c(1,1,1))
+abline(h=0,lty=c(3))
