610a3341537b0f417235e9e5f8857d82820f68a6
Nicolas Iglesias <nico@webpolis.com.ar>
2014-10-18 19:03:10 -0300

    turn ROC to discrete; remove real

--- server/scripts/cftc.r
+++ server/scripts/cftc.r
@@ -13,22 +13,22 @@ data[,1] = gsub("(.*)\\s+\\-\\s+.*","\\1",data[,1],ignore.case=T,perl=T)
 
 getPosition <- function(currency){
 	curr = subset(data,Market.and.Exchange.Names==currency)
-	cl = ROC(rev(curr$Noncommercial.Positions.Long..All.))
-	cs = ROC(rev(curr$Noncommercial.Positions.Short..All.))
-	ci = ROC(rev(curr$Open.Interest..All.))
+	cl = ROC(rev(curr$Noncommercial.Positions.Long..All.),type="discrete")
+	cs = ROC(rev(curr$Noncommercial.Positions.Short..All.),type="discrete")
+	ci = ROC(rev(curr$Open.Interest..All.),type="discrete")
 	pos = matrix(c(cl,cs,ci),ncol=3,dimnames=list(NULL,c("long","short","interest")))
 	ret = last(zoo(pos))
 	index(ret) = c(currency)
 	return(ret)
 }
 
-currencies = c("CANADIAN DOLLAR","SWISS FRANC","BRITISH POUND STERLING","JAPANESE YEN","EURO FX","BRAZILIAN REAL","NEW ZEALAND DOLLAR","AUSTRALIAN DOLLAR","U.S. DOLLAR INDEX","NIKKEI STOCK AVERAGE")
+currencies = c("CANADIAN DOLLAR","SWISS FRANC","BRITISH POUND STERLING","JAPANESE YEN","EURO FX","NEW ZEALAND DOLLAR","AUSTRALIAN DOLLAR","U.S. DOLLAR INDEX","NIKKEI STOCK AVERAGE")
 
 stats = Reduce(rbind,lapply(currencies,getPosition))
 
 unlink(tmp)
 unlink(reportName)
 
-barplot(as.matrix(t(stats)),main="COT - Rate of Change",ylab="Positioning",beside=T,col=c("green","red","blue"),cex.names=0.35)
+barplot(as.matrix(t(stats)),main="COT - Rate of Change",ylab="Positioning",beside=T,col=c("green","red","blue"),cex.names=0.4)
 legend("bottomright", c("long","short","interest"), cex=0.8,col=c("green","red","blue"), lty=c(1,1,1))
 grid()
