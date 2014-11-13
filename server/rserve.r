a04b71473e000bad04f4a6094093efc1db8ac905
Nicolas Iglesias <nico@webpolis.com.ar>
2014-11-13 12:18:18 -0300

    allow switch rserve debug; rename mobile app

--- server/rserve.r
+++ server/rserve.r
@@ -4,4 +4,7 @@ pwd = ifelse(is.null(sys.frames()),getwd(),paste(dirname(sys.frame(1)$ofile),"/.
 header = paste(pwd,"server","scripts","header.r",sep="/")
 socket = paste(pwd,"server","socket",sep="/")
 
-Rserve(debug=TRUE,args=paste("--RS-enable-control","--vanilla","--RS-source",header,"--RS-workdir",paste(pwd,"/",sep=""),"--RS-socket",socket,sep=" "))
+opts = commandArgs(trailingOnly = TRUE)
+isDebug = as.integer(opts[1])
+
+Rserve(debug=isDebug,args=paste("--RS-enable-control","--vanilla","--RS-source",header,"--RS-workdir",paste(pwd,"/",sep=""),"--RS-socket",socket,sep=" "))
