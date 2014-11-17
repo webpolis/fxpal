6294156cffccf0707f508286bea7de8307305635
Nicolas Iglesias <nico@webpolis.com.ar>
2014-11-16 22:09:25 -0300

    enclose curr

--- server/api.js
+++ server/api.js
@@ -354,7 +354,7 @@ server.get('/api/cot/:cross/:currency1/:currency2', function respond(req, res, n
     var sinceMinutes = 61;
     // only generate file if it's older than XX minutes
     if (isOutdatedFile(outFile, sinceMinutes)) {
-        sh.run(['Rscript', __dirname + '/scripts/positioning.r', instrument, req.params.currency1, req.params.currency2].join(' '));
+        sh.run(['Rscript', __dirname + '/scripts/positioning.r', instrument, '"' + req.params.currency1 + '"', '"' + req.params.currency2 + '"'].join(' '));
         fs.stat(outFile, function(err, stat) {
             var img = fs.readFileSync(outFile);
             res.contentType = 'image/png';
