30c172ae0971ffab5324ed0f336449d44a761f4e
Nicolas Iglesias <nico@webpolis.com.ar>
2014-12-06 17:44:08 -0300

    make social clust method; chg txt

--- server/scripts/social.r
+++ server/scripts/social.r
@@ -1,23 +1,31 @@
 library(tm)
 library(twitteR)
 
-load("twitterCredentials.RData")
+Sys.setenv(TZ='UTC')
+
+pwd = getwd()
+dataPath = paste(pwd,'/app/data/',sep='')
+
+load(paste(dataPath,'twitterCredentials.RData',sep='/'))
 registerTwitterOAuth(twitCred)
 
-twits = searchTwitter("GBPUSD",n=500,lang="en")
-df = do.call("rbind", lapply(twits, as.data.frame))
-corpus = Corpus(VectorSource(df$text))
+getCluster <- function(cross=NA){
+	twits = searchTwitter(cross,n=500,lang="en")
+	df = do.call('rbind', lapply(twits, as.data.frame))
+	corpus = Corpus(VectorSource(df$text))
 
-corpus = tm_map(corpus,content_transformer(tolower))
-corpus = tm_map(corpus,content_transformer(removePunctuation))
-corpus = tm_map(corpus,content_transformer(removeNumbers))
-corpus = tm_map(corpus,removeWords,stopwords("english"))
-#corpus = tm_map(corpus,stemDocument)
+	corpus = tm_map(corpus,content_transformer(tolower))
+	corpus = tm_map(corpus,content_transformer(removePunctuation))
+	corpus = tm_map(corpus,content_transformer(removeNumbers))
+	corpus = tm_map(corpus,removeWords,stopwords('english'))
+	#corpus = tm_map(corpus,stemDocument)
 
-dtm = TermDocumentMatrix(corpus, control = list(minWordLength = 1))
-dtmSparse = removeSparseTerms(dtm,0.9)
-dfSparse = as.data.frame(inspect(dtmSparse))
-scaleSparse = scale(dfSparse)
-distSparse = dist(scaleSparse, method = "euclidean")
-clustSparse = hclust(distSparse, method="ward")
+	dtm = TermDocumentMatrix(corpus, control = list(minWordLength = 1))
+	dtmSparse = removeSparseTerms(dtm,0.9)
+	dfSparse = as.data.frame(inspect(dtmSparse))
+	scaleSparse = scale(dfSparse)
+	distSparse = dist(scaleSparse, method = 'euclidean')
+	clustSparse = hclust(distSparse, method='ward')
 
+	return(clustSparse)
+}
