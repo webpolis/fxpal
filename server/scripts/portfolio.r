98b8a235f60937052e721b6771eeaf9135cd033f
Nicolas Iglesias <nico@webpolis.com.ar>
2015-03-08 11:10:21 -0300

    add newline, increase portfolio filter

--- server/scripts/portfolio.r
+++ server/scripts/portfolio.r
@@ -45,7 +45,7 @@ mediumWeights = sort(mediumWeights, decreasing = TRUE)
 #barplot(pf, cex.names = 0.34)
 
 out = data.frame(cross = names(mediumWeights), percentage = mediumWeights)
-out = out[out$percentage>0.006|out$percentage< -0.005,]
+out = out[out$percentage>0.01|out$percentage< -0.01,]
 write.csv(out, quote = FALSE, row.names = FALSE, file = "portfolio.csv", fileEncoding = "UTF-8")
 
 quit()
