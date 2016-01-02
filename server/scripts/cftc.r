6b497b02ee5178b122518dc89f1ee9fbb99e3fe7
Nicolas Iglesias <nico@webpolis.com.ar>
2016-01-02 16:41:52 -0300

    keep cot as of 2015; fix index for data

--- server/scripts/cftc.r
+++ server/scripts/cftc.r
@@ -3,14 +3,15 @@ source(paste(pwd,'server','scripts','header.r',sep='/'))
 
 week = format(Sys.time(),'%U');
 month = format(Sys.time(),'%m');
-year = as.integer(format(Sys.time(),'%Y'));
-data = getCOTData()
+year = 2015#as.integer(format(Sys.time(),'%Y'));
+data = getCOTData(1)
 
 markets = list('CAD'='CANADIAN DOLLAR','CHF'='SWISS FRANC','GBP'='BRITISH POUND STERLING','JPY'='JAPANESE YEN','EUR'='EURO FX','NZD'='NEW ZEALAND DOLLAR','AUD'='AUSTRALIAN DOLLAR','USD'='U.S. DOLLAR INDEX','NIKKEI'='NIKKEI STOCK AVERAGE');
 markets = as.list(as.data.frame(markets)[order(as.data.frame(markets))])
 
 stats = Reduce(rbind,lapply(markets,getCOTPosition,data));
-index(stats) <- names(markets) #c('AUD','GBP','CAD','EUR','JPY','NZD','NIKKEI','CHF','USD');
+stats = as.data.frame(stats)
+rownames(stats) <- names(markets) #c('AUD','GBP','CAD','EUR','JPY','NZD','NIKKEI','CHF','USD');
 
 jpeg(paste(dataPath,'cot/COT', '-',year, '.jpg', sep = ''),width=1334,height=750,quality=100,bg='dimgray');
 barplot(as.matrix(t(stats)),ylab='Positioning',beside=T,col=c('olivedrab4','firebrick3','dodgerblue4'),cex.names=2,col.lab='white',col.axis='white',main='COT Status',col.main='white');
