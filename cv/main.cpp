5327471ddd17220daaa2c9b8c3009646283a9e70
Nicolas Iglesias <nfiglesias@gmail.com>
2016-10-03 01:36:18 -0300

    match shapes

--- cv/main.cpp
+++ cv/main.cpp
@@ -9,67 +9,38 @@ using namespace std;
 using namespace cv;
 
 int lines(const char*);
-bool drawChart(const char*, const int, const char*);
+bool drawCandles(const char*, const int, const int, const char*);
+vector<Point> getCornerPoints(Mat);
+vector<vector<Point> > getContourFromPoints(vector<Point>);
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
+        // compose charts
         const char* csv = argv[1];
-        const int pLast = 10;
-        const int pTotal = lines(csv) - 1;
+        int pLast = 110;
+        int pTotal = lines(csv) - 1;
         string cLast = string(csv) + to_string(pLast) + string(".png");
         string cTotal = string(csv) + to_string(pTotal) + string(".png");
 
-        drawChart(csv, pLast, cLast.c_str());
-        drawChart(csv, pTotal/3, cTotal.c_str());
+        drawCandles(csv, 100, pLast, cLast.c_str());
+        drawCandles(csv, 0, 10, cTotal.c_str());
 
+        // load up charts in grey scale
         Mat img1 = imread(cLast.c_str(), IMREAD_GRAYSCALE);
         Mat img2 = imread(cTotal.c_str(), IMREAD_GRAYSCALE);
 
+        // get corner points
+        vector<Point> cornerPoints1 = getCornerPoints(img1);
+        vector<Point> cornerPoints2 = getCornerPoints(img2);
 
-        int thresh = 200;
-        int max_thresh = 255;
-        Mat dst, dst_norm, dst_norm_scaled;
-        dst = Mat::zeros( img1.size(), CV_32FC1 );
-        vector<Point> points;
+        // convert shape points into contour sequence
+        vector<vector<Point> > shapeContour1 = getContourFromPoints(cornerPoints1);
+        vector<vector<Point> > shapeContour2 = getContourFromPoints(cornerPoints2);
 
-/// Detector parameters
-        int blockSize = 2;
-        int apertureSize = 3;
-        double k = 0.04;
+        double sh = matchShapes(shapeContour1.at(0), shapeContour2.at(0), CV_CONTOURS_MATCH_I1, 0);
+        cout << sh << endl;
 
-/// Detecting corners
-        cornerHarris( img1, dst, blockSize, apertureSize, k, BORDER_DEFAULT );
-
-/// Normalizing
-        normalize( dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
-        convertScaleAbs( dst_norm, dst_norm_scaled );
-
-/// Drawing a circle around corners
-        for( int j = 0; j < dst_norm.rows; j++ )
-        { for( int i = 0; i < dst_norm.cols; i++ )
-          {
-                  if( (int) dst_norm.at<float>(j,i) > thresh )
-                  {
-                          points.push_back(Point( i, j ));
-                  }
-          }}
-
-        vector<int> hull; int i;
-        convexHull(Mat(points), hull, true);
-
-        for( i = 0; i < points.size(); i++ )
-                circle(img1, points[i], 3, Scalar(0, 0, 255), FILLED, LINE_AA);
-
-        int hullcount = (int)hull.size();
-        Point pt0 = points[hull[hullcount-1]];
-
-        for( i = 0; i < hullcount; i++ )
-        {
-                Point pt = points[hull[i]];
-                line(img1, pt0, pt, Scalar(0, 255, 0), 1,LINE_AA);
-                pt0 = pt;
-        }
-
-        imwrite("out.png", img1);
+        drawContours(img1, shapeContour1, -1, Scalar(0,255,0), 1);
+        imwrite("out1.png", img1);
 
         return 0;
 }
@@ -85,12 +56,13 @@ int lines(const char* filename){
         return number_of_lines;
 }
 
-bool drawChart(const char* fname, const int tail, const char* cname){
-        const int w = tail > 10 ? tail * 9 : 320;
-        const int h = tail > 10 ? ceil(w/2) : 240;
+bool drawCandles(const char* fname, const int start, const int end, const char* cname){
+        double candleDimRatio = (end-start)*15;
+        const int w = ceil(candleDimRatio);
+        const int h = ceil(candleDimRatio/1.3333);
 
         // load data
-        const int rows = tail; //lines(fname) - 1;
+        int rows = end-start;
         io::CSVReader<5> in(fname);
 
         double highData[rows];
@@ -101,17 +73,20 @@ bool drawChart(const char* fname, const int tail, const char* cname){
         in.read_header(io::ignore_extra_column, "date", "Open", "High", "Low", "Close");
         string date; double open; double high; double low; double close;
 
-        int r = 0;
-        while(in.read_row(date, open, high, low, close)) {
-                highData[r] = high;
-                lowData[r] = low;
-                openData[r] = open;
-                closeData[r] = close;
-                //cout << highData[r] << "," << lowData[r] << "," << openData[r] << "," << closeData[r] << "," << endl;
-                r++;
+        int r = 0; int rr = 0;
 
-                if(r>=tail)
-                        break;
+        cout << "start-end "<<start<<","<<end<<" printing "<<rows<<endl;
+
+        while(in.read_row(date, open, high, low, close)) {
+                if(r >= start && r < end) {
+                        highData[rr] = high;
+                        lowData[rr] = low;
+                        openData[rr] = open;
+                        closeData[rr] = close;
+                        //cout << highData[rr] << "," << lowData[rr] << "," << openData[rr] << "," << closeData[rr] << "," << endl;
+                        rr++;
+                }
+                r++;
         }
 
         // Create a XYChart object of size 600 x h pixels
@@ -142,4 +117,56 @@ bool drawChart(const char* fname, const int tail, const char* cname){
         Rect roi(0,0,w,h-10);
         Mat cropped(img, roi);
         imwrite(cname, cropped);
+}
+
+vector<Point> getCornerPoints(Mat img){
+        int thresh = 200;
+        int max_thresh = 255;
+        Mat dst, dst_norm, dst_norm_scaled;
+        dst = Mat::zeros(img.size(), CV_32FC1);
+        vector<Point> points;
+
+        // Detector parameters
+        int blockSize = 2;
+        int apertureSize = 3;
+        double k = 0.04;
+
+        // Detecting corners
+        cornerHarris(img, dst, blockSize, apertureSize, k, BORDER_DEFAULT);
+
+        // Normalizing
+        normalize(dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
+        convertScaleAbs(dst_norm, dst_norm_scaled);
+
+        // Drawing a circle around corners
+        for(int j = 0; j < dst_norm.rows; j++)
+        { for(int i = 0; i < dst_norm.cols; i++)
+          {
+                  if((int) dst_norm.at<float>(j,i) > thresh)
+                  {
+                          points.push_back(Point(i, j));
+                  }
+          }}
+
+        return points;
+}
+
+vector<vector<Point> > getContourFromPoints(vector<Point> points){
+        vector<Point> shapePoints;
+        vector<int> hull; int i;
+        convexHull(Mat(points), hull, true);
+
+        int hullcount = (int)hull.size();
+
+        for(i = 0; i < hullcount; i++)
+        {
+                Point pt = points[hull[i]];
+                shapePoints.push_back(pt);
+        }
+
+        // convert shape points into contour sequence
+        vector<vector<Point> > shapeContour;
+        shapeContour.push_back(shapePoints);
+
+        return shapeContour;
 }
