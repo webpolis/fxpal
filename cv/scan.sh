cab84b9da7986c5430c9ade7617d0aee9283d16e
Nicolás Iglesias <nfiglesias@gmail.com>
2024-03-17 23:07:23 -0300

    updates

--- cv/scan.sh
+++ cv/scan.sh
@@ -4,5 +4,6 @@ MIN=$1
 MAX=$2
 CSV_TPL=$3 # current data
 CSV_SAMPLE=$4 # historical data where to find similar patterns
+DIST_MAX=$5
 
-for i in $(seq $MIN $MAX); do ./CVMatcher $CSV_TPL $CSV_SAMPLE $i; done
+for i in $(seq $MIN $MAX); do ./CVMatcher $CSV_TPL $CSV_SAMPLE $i $DIST_MAX; done
